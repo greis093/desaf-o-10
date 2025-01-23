22ee340315163da49bc1b471fef7f9a814cb60d8
Greicy Agreda <yirley093@gmail.com>
2025-01-22 19:25:42 -0500

    actualizacion con vite

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { resolve } from "path";
 // https://vite.dev/config/
 // https://vite.dev/config/shared-options.html#css-devsourcemap
 // vite.config.js
-export default {
-  // config options
-  css: {
-    devSourcemap: true,
-  },
-  build: {
-    rollupOptions: {},
-  },
-};
+export default {};
