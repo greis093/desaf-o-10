00fe3a11f14af47edd0744407183c3c2a20d63cd
Greicy Agreda <yirley093@gmail.com>
2025-01-22 19:21:58 -0500

    elimine archivos del vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
-import { resolve } from 'path'
+import { resolve } from "path";
 // https://vite.dev/config/
 // https://vite.dev/config/shared-options.html#css-devsourcemap
 // vite.config.js
 export default {
-    // config options
-    css: {
-        devSourcemap: true
-    },
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                contacto: resolve(__dirname, 'pages/contacto/index.html'),
-                nosotros: resolve(__dirname, 'pages/nosotros/index.html'),
-                productos: resolve(__dirname, 'pages/productos/index.html'),
-                servicios: resolve(__dirname, 'pages/servicios/index.html'),
-            }
-        }
-    }
-}
+  // config options
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    rollupOptions: {},
+  },
+};
